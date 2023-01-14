a06f7b97db0dddf5bbb2302047e2e4e8077b9b35
nahomypolycar <112959201+nahomypolycar@users.noreply.github.com>
2023-01-14 17:17:19 +0100

    problem import images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div>
+      <header>
+        <img
+          src="/Users/nahomy/Formation/REACT/JOUR6/Projet-Deliveroo/deliveroo-frontend/deliveroo-app/src/Assets/images/logo-teal.svg"
+          alt="logo"
+        />
+      </header>
       <h1>Hello World</h1>
     </div>
   );
